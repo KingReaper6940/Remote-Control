b2eb9f937c3a66319b9a34b6a0b22066a825aa4a
Vrishn Viswa Sathyamoorthy <viscoolsthemass@gmail.com>
2026-04-25 22:28:25 -0400

    fix: normalize convex public urls

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,10 @@
+function stripTrailingSlash(value: string) {
+  return value.replace(/\/+$/, "");
+}
+
 export const publicClerkPublishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ?? "";
-export const publicConvexUrl = process.env.NEXT_PUBLIC_CONVEX_URL ?? "";
-export const publicConvexSiteUrl = process.env.NEXT_PUBLIC_CONVEX_SITE_URL ?? "";
+export const publicConvexUrl = stripTrailingSlash(process.env.NEXT_PUBLIC_CONVEX_URL ?? "");
+export const publicConvexSiteUrl = stripTrailingSlash(process.env.NEXT_PUBLIC_CONVEX_SITE_URL ?? "");
 
 export const hasPublicClerkEnv = Boolean(publicClerkPublishableKey);
 export const hasPublicConvexEnv = Boolean(publicConvexUrl && publicConvexSiteUrl);
